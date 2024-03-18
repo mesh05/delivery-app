7d1264610a0dae4fa377f410e55359f31bcbe05f
Prathamesh <pr.sawant.in@gmail.com>
2024-03-18 23:07:40 +0530

    v0.2.1 changes in stall frontend

--- stall/src/Dashboard.jsx
+++ stall/src/Dashboard.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Card from "@mui/material/Card";
+import { Button, Typography } from "@mui/material";
 
 function Dashboard() {
   const [orders, setOrders] = useState([]);
@@ -16,27 +18,42 @@ function Dashboard() {
   //   );
   // };
 
-  
   useEffect(() => {
-    axios.get("http://localhost:3000/stall/realtime_orders")
-    .then((response) => {
-      // console.log(response.data.placedOrders);
-      setOrders(response.data.placedOrders);
-      // const eventSource = new EventSource(`http://localhost:3000/stall/realtime_orders`);
-      // eventSource.onmessage = (e) => updateOrders(e.data);
-      // return () => {
-      //   eventSource.close();
-      // }
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-    
+    axios
+      .get("http://localhost:3000/stall/realtime_orders")
+      .then((response) => {
+        // console.log(response.data.placedOrders);
+        setOrders(response.data.placedOrders);
+        // const eventSource = new EventSource(`http://localhost:3000/stall/realtime_orders`);
+        // eventSource.onmessage = (e) => updateOrders(e.data);
+        // return () => {
+        //   eventSource.close();
+        // }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }, []);
   return (
     <div>
       <h1>Dashboard</h1>
-      {JSON.stringify(orders)}
+      <ul>
+        {orders.map((order) => (
+          <Card key={order.id}>
+            <Typography variant="h5">Order ID: {order.orderId}</Typography>
+            <br></br>
+            <br></br>
+            <Button variant="contained" sx={{ backgroundColor: "green" }}>
+              delivered
+            </Button>
+            <br></br>
+            <br></br>
+            <Button variant="contained" sx={{ backgroundColor: "red" }}>
+              Cancel order
+            </Button>
+          </Card>
+        ))}
+      </ul>
     </div>
   );
 }
