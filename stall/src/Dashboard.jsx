fe855b44ddf70cf8d3a05dfa3c4fd213b630f5bb
Prathamesh <pr.sawant.in@gmail.com>
2024-03-21 15:50:50 +0530

    v0.6.5

--- stall/src/Dashboard.jsx
+++ stall/src/Dashboard.jsx
@@ -3,42 +3,56 @@ import axios from "axios";
 import Card from "@mui/material/Card";
 import { Button, Typography } from "@mui/material";
 import io from "socket.io-client";
-const socket = io.connect("http://ruchulu.live:3000");
+const socket = io.connect("https://ruchulu.live");
 
 function Dashboard() {
   const [orders, setOrders] = useState([]);
   const [text, setText] = useState("");
 
-  // const updateOrders = (data) => {
-  //   const parsedData = JSON.parse(data);
-  //   setOrders((orders) =>
-  //     [...orders].map((order) => {
-  //       if (order.id === parsedData.id) {
-  //         return parsedData;
-  //       }
-  //       return order;
-  //     })
-  //   );
-  // };
-
   useEffect(() => {
     // Have to hit api that makes DB calls for initial orders
-    socket.on("new_order", (data) => {
-      setOrders(data.placedOrders);
-    });
-    socket.on("order", (data) => {
-      setText(data.message);
-    });
     axios
       .get("https://ruchulu.live/api/stall/orders")
       .then((response) => {
-        console.log(response.data.placedOrders);
         setOrders(response.data.placedOrders);
       })
       .catch((error) => {
         console.log(error);
       });
-  }, [socket]);
+  }, []);
+
+  useEffect(() => {
+    socket.on("new_order", (data) => {
+      setOrders([...orders, data.order]);
+    });
+
+    socket.on("order_cancelled", (data) => {
+      console.log(data);
+      // setOrders(
+      //   orders.map((order) => {
+      //     if (order.orderId === data.order.orderId) {
+      //       return data.order;
+      //     }
+      //     return order;
+      //   })
+      // );
+    });
+
+    socket.on("order_delivered", (data) => {
+      console.log(data);
+      // setOrders(
+      //   orders.map((order) => {
+      //     if (order.orderId === data.order.orderId) {
+      //       return data.order;
+      //     }
+      //     return order;
+      //   })
+      // );
+    });
+    // socket.on("order", (data) => {
+    //   setText(data.message);
+    // });
+  }, [orders]);
   return (
     <div>
       <h1>Dashboard</h1>
@@ -53,14 +67,10 @@ function Dashboard() {
           <br></br>
           <Button
             onClick={() => {
-              axios
-                .put("https://ruchulu.live/api/orderDelivered", {
-                  orderId: order.orderId,
-                  status: "delivered",
-                })
-                .then((response) => {
-                  console.log(response);
-                });
+              socket.emit("order_delivered", {
+                orderId: order.orderId,
+                status: "delivered",
+              });
             }}
             variant="contained"
             sx={{ backgroundColor: "green" }}
