521416cca5144d6cae4e3a63a023132045b3e44f
Prathamesh <pr.sawant.in@gmail.com>
2024-03-23 12:38:36 +0530

    v1.3

--- client/src/recoil/atoms/atoms.jsx
+++ client/src/recoil/atoms/atoms.jsx
@@ -3,18 +3,18 @@ import { atom, selector } from "recoil";
 const stallDetailState = atom({
   key: "stallDetailState", // unique ID (with respect to other atoms/selectors)
   default: [
-    { id: 1, Name: "Chicken Pizza", image: "/1.png", Price: 200.0 },
-    { id: 2, Name: "Margherita Pizza", image: "/2.png", Price: 140.0 },
+    { id: 1, Name: "Chicken Pizza", image: "/1.png", Price: 220.0 },
+    { id: 2, Name: "Margherita Pizza", image: "/2.png", Price: 150.0 },
     {
       id: 3,
       Name: "Paneer Tikka Pizza",
       image: "/3.png",
-      Price: 200.0,
+      Price: 220.0,
     },
     { id: 4, Name: "Chicken Burger", image: "/4.png", Price: 140.0 },
     { id: 5, Name: "Veg Popular", image: "/veg-popular.jpeg", Price: 130.0 },
-    { id: 6, Name: "Salted Fries", image: "/6.png", Price: 100.0 },
-    { id: 7, Name: "Peri Peri Fries", image: "/7.png", Price: 120.0 },
+    { id: 6, Name: "Salted Fries", image: "/6.png", Price: 120.0 },
+    { id: 7, Name: "Peri Peri Fries", image: "/7.png", Price: 140.0 },
     {
       id: 8,
       Name: "Chicken Loaded Cheese Fries",
@@ -25,25 +25,25 @@ const stallDetailState = atom({
       id: 9,
       Name: "Chicken Strips Supreme (4-pieces)",
       image: "/9.png",
-      Price: 120.0,
+      Price: 140.0,
     },
     {
       id: 9,
       Name: "Backlog Combo (Chicken/Veg Pizza + Salted Fries + Coke)",
       image: "/backlog-combo.jpeg",
-      Price: 320.0,
+      Price: 340.0,
     },
     {
       id: 10,
       Name: "Bunk Combo (Chicken/Veg Burger + Salted Fries + Coke)",
       image: "/bunk-combo.jpeg",
-      Price: 270.0,
+      Price: 290.0,
     },
     {
       id: 11,
       Name: "Stress Combo (Chicken Strips + Burger + Coke)",
       image: "/stress-combo.jpg",
-      Price: 290.0,
+      Price: 300.0,
     },
   ],
 });
